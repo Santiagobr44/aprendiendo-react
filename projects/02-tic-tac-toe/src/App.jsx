2aa2095180311a4c8c1242c78b294396bc88bc4f
Santiagobr44 <santiagobr.44@gmail.com>
2025-01-04 13:07:07 -0500

    Extract to components tic tac toe

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,34 +1,10 @@
 import { useState } from "react";
-
-const TURNS = {
-    X: "x",
-    O: "o",
-};
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-    const className = `square ${isSelected ? "is-selected" : ""}`;
-
-    const handleClick = () => {
-        updateBoard(index);
-    };
-
-    return (
-        <div onClick={handleClick} className={className}>
-            {children}
-        </div>
-    );
-};
-
-const WINNER_COMBOS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
+import confetti from "canvas-confetti";
+import { Square } from "./components/Square";
+import { TURNS } from "./constants.js";
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
+import { WinnerModal } from "./components/WinnerModal.jsx";
+import { Board } from "./components/Board.jsx";
 
 function App() {
     const [board, setBoard] = useState(Array(9).fill(null));
@@ -37,20 +13,10 @@ function App() {
 
     const [winner, setWinner] = useState(null);
 
-    const checkWinner = (boardToCheck) => {
-        // revisamos todas las combinaciones ganadoras
-        // para ver si X u O gano
-        for (const combo of WINNER_COMBOS) {
-            const [a, b, c] = combo;
-            if (
-                boardToCheck[a] &&
-                boardToCheck[a] == boardToCheck[b] &&
-                boardToCheck[a] == boardToCheck[c]
-            ) {
-                return boardToCheck[a];
-            }
-        }
-        return null;
+    const resetGame = () => {
+        setBoard(Array(9).fill(null));
+        setTurn(TURNS.X);
+        setWinner(null);
     };
 
     const updateBoard = (index) => {
@@ -65,33 +31,26 @@ function App() {
         const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
         setTurn(newTurn);
         // revisar si hay ganador
-        const newWinner = checkWinner(newBoard);
+        const newWinner = checkWinnerFrom(newBoard);
         if (newWinner) {
+            confetti();
             setWinner(newWinner);
+        } else if (checkEndGame(newBoard)) {
+            setWinner(false); // empate
         }
     };
 
     return (
         <main className="board">
             <h1>Tic tac toe</h1>
-            <section className="game">
-                {board.map((_, index) => {
-                    return (
-                        <Square
-                            key={index}
-                            index={index}
-                            updateBoard={updateBoard}
-                        >
-                            {board[index]}
-                        </Square>
-                    );
-                })}
-            </section>
-
+            <button onClick={resetGame}>Reset del juego</button>
+            <Board board={board} updateBoard={updateBoard} />
             <section className="turn">
                 <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
                 <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
             </section>
+
+            <WinnerModal winner={winner} resetGame={resetGame} />
         </main>
     );
 }
